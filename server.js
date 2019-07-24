8557a8fa9c0d4ee3612072d82126d9572dc4b375
bklyn <ljoconnor5@gmail.com>
2019-07-24 11:34:30 -0400

    wkjshdfksd

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var http = require('http');
+// var fs = require('fs');
+// var http = require('http');
 var express = require('express')
 const path = require('path')
 var app = express();
@@ -8,11 +8,12 @@ var app = express();
 var port = process.env.PORT || 8080;
 //Important. Listening on a port like 3000 makes your app available only on your local server. In a production context, you want the app to be accessed on whatever port the server is listening on. That will be represented by the server's global object: process.env.PORT. If that doesn't exist because you're not in production, it'll default to 3000.
 
-app.use('/static', express.static('./public'))
+// app.use('/static', express.static('./public'))
 //when someone makes a request to the path passed to use, express provides access to the files in the public folder
 
 app.get('/', function(req, res){
-	res.sendFile('./src/index.html', {root: __dirname });
+	res.sendFile('src/index.html', {root: __dirname });
+	// res.send('Hey man');
 	//you don't need to specify a content-type. the express methods and express response object (wrapper around http_parser res object) looks at what you're sending and takes care of it.
 })
 //this is the routing part. It gives you the request and response objects to work with. When you receive a GET request to /, do this...
